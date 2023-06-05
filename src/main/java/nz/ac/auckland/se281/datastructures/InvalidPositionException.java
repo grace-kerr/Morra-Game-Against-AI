092353a0d239f0ebe93eea2969b9ec029773839a
grace-kerr <gker710@aucklanduni.ac.nz>
2023-06-05 20:40:35 +1200

    made a invalid position expection linkedlist

---
+++ src/main/java/nz/ac/auckland/se281/datastructures/InvalidPositionException.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se281.datastructures;
+
+public class InvalidPositionException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public InvalidPositionException() {}
+
+  public InvalidPositionException(String message) {
+    super(message);
+  }
+}
