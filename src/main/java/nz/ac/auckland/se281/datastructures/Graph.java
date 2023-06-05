76fe2639712204c4fe2ec6593594cb9f9b008bb0
grace-kerr <gker710@aucklanduni.ac.nz>
2023-06-05 21:44:06 +1200

    isReflexive all working :)

--- src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
@@ -1,7 +1,6 @@
 package nz.ac.auckland.se281.datastructures;
 
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,65 +17,88 @@ import java.util.Set;
  */
 public class Graph<T extends Comparable<T>> {
 
-  private Set<T> verticies;
+  private Set<T> vertices;
   private Set<Edge<T>> edges;
   private Map<T, LinkedList<Edge<T>>> adjacencyMap;
 
-  // private Set<T> roots;
-
   public Graph(Set<T> verticies, Set<Edge<T>> edges) {
     // initialize the graph with the given verticies and edges
-    this.verticies = verticies;
+    this.vertices = verticies;
     this.edges = edges;
     adjacencyMap = new HashMap<T, LinkedList<Edge<T>>>();
-    // this.roots = new HashSet<>();
+
+    // // Initialize adjacency map with empty lists for each vertex
+    // for (T vertex : vertices) {
+    //   adjacencyMap.put(vertex, new LinkedList<Edge<T>>());
+    // }
+
+    // // Populate adjacency map with edges
+    // for (Edge<T> edge : edges) {
+    //   T source = edge.getSource();
+    //   adjacencyMap.get(source).add(edge);
+    // }
   }
 
   public Set<T> getRoots() {
-    Set<T> roots = new LinkedHashSet<>();
+    // Set<T> roots = new LinkedHashSet<>();
 
-    for (Map.Entry<T, LinkedList<Edge<T>>> entry : adjacencyMap.entrySet()) {
-      T vertex = entry.getKey();
-      LinkedList<Edge<T>> edges = entry.getValue();
+    // for (Map.Entry<T, LinkedList<Edge<T>>> entry : adjacencyMap.entrySet()) {
+    //   T vertex = entry.getKey();
+    //   LinkedList<Edge<T>> edges = entry.getValue();
 
-      boolean hasOutgoingEdges = false;
+    //   boolean hasOutgoingEdges = false;
 
-      for (Edge<T> edge : edges) {
-        if (edge.getSource().equals(vertex)) {
-          hasOutgoingEdges = true;
-          break;
-        }
-      }
+    //   for (Edge<T> edge : edges) {
+    //     if (edge.getSource().equals(vertex)) {
+    //       hasOutgoingEdges = true;
+    //       break;
+    //     }
+    //   }
 
-      if (!hasOutgoingEdges) {
-        // Vertex has no outgoing edges
-        continue;
-      }
+    //   if (!hasOutgoingEdges) {
+    //     // Vertex has no outgoing edges
+    //     continue;
+    //   }
 
-      boolean hasIncomingEdges = false;
+    //   boolean hasIncomingEdges = false;
 
-      for (LinkedList<Edge<T>> otherEdges : adjacencyMap.values()) {
-        for (Edge<T> edge : otherEdges) {
-          if (edge.getDestination().equals(vertex) && !edge.getSource().equals(vertex)) {
-            hasIncomingEdges = true;
-            break;
-          }
-        }
-        if (hasIncomingEdges) {
-          break;
-        }
-      }
+    //   for (LinkedList<Edge<T>> otherEdges : adjacencyMap.values()) {
+    //     for (Edge<T> edge : otherEdges) {
+    //       if (edge.getDestination().equals(vertex) && !edge.getSource().equals(vertex)) {
+    //         hasIncomingEdges = true;
+    //         break;
+    //       }
+    //     }
+    //     if (hasIncomingEdges) {
+    //       break;
+    //     }
+    //   }
 
-      if (hasIncomingEdges) {
-        // Vertex has at least 1 incoming edge and 1 outgoing edge
-        // Do something with the vertex
-      }
-    }
+    //   if (hasIncomingEdges) {
+    //     // Vertex has at least 1 incoming edge and 1 outgoing edge
+    //     // Do something with the vertex
+    //   }
+    // }
+    throw new UnsupportedOperationException();
   }
 
   public boolean isReflexive() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    for (T vertex : vertices) {
+      boolean hasSelfLoop = false;
+
+      for (Edge<T> edge : edges) {
+        if (edge.getSource().equals(vertex) && edge.getDestination().equals(vertex)) {
+          hasSelfLoop = true;
+          break;
+        }
+      }
+
+      if (!hasSelfLoop) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   public boolean isSymmetric() {
